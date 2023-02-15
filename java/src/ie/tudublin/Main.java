db3fbcd7d0dc9ac767eedf4d00b1ebd1fff9aeb5
nikkamay <C21359691@mytudublin.ie>
2023-02-15 17:28:04 +0000

    Loops

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,11 +3,6 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
 
 	public static void BugZap()
 	{

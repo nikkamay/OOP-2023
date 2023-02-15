53dda372754e8a9ffedb3538b89a1d8cc9278876
nikkamay <C21359691@mytudublin.ie>
2023-02-15 17:26:08 +0000

    BugZap

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,12 +3,6 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
 	public static void BugZap()
 	{
 		String[] a = {"MAIN"};
@@ -17,11 +11,7 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
-		
-		Dog penny = new Dog();
-		penny.setName("Penny");
-		penny.speak();
+
 
 		BugZap();
 	}

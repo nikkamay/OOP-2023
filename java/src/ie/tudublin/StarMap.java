033de2dc66651937b86fe99a9a4b1559759aeaa5
nikkamay <C21359691@mytudublin.ie>
2023-02-22 11:41:16 +0000

    stars lab4

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,51 +1,136 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
-public class StarMap extends PApplet
-{
-	public void settings()
-	{
-		size(800, 800);
-	}
+public class StarMap extends PApplet {
 
-	public void setup() {
-		colorMode(RGB);
-		background(0);
+    ArrayList<Star> stars = new ArrayList<Star>();
+    
+    public float border;
 
-		smooth();
-		
-	}
+    void drawGrid()
+    {
+        stroke(255, 0, 255);
+        textAlign(CENTER, CENTER);
+        textSize(20);
+        for(int i = -5; i <=5; i ++)
+        {
+            float x = map(i, -5, 5, border, width - border);
+            line(x, border, x, height - border);
+            line(border, x, width - border, x);
+            fill(255);
+            text(i, x, border * 0.5f);
+            text(i, border * 0.5f, x);
+        }
+    }
 
-	public void drawGrid()
-	{
-		float border = 50;
-		fill(255,0,255);
-		stroke(255,0,255);
-		textAlign(CENTER, CENTER);
-		for(float x = -5 ; x <=5; x ++)
-		{
-			float lx = map(x, -5, 5, border, width - border);
-			line(lx, border, lx, height - border);
-			text((int) x, lx, 10);
+    void printStars()
+    {
+        for(Star s:stars)
+        {
+            System.out.println(s);
+        }
+    }
+
+    void loadStars()
+    {
+        Table table = loadTable("HabHYG15ly.csv", "header");
+        for(TableRow r:table.rows())
+        {
+            Star s = new Star(r);
+            stars.add(s);
+        }
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    Star first = null;
+    Star second = null;
 
 
-		}
 
-		for(float y = -5 ; y <=5; y ++)
-		{
-		  float ly = map(y, -5, 5, border, height - border);
-		  line(border, ly, width - border, ly);
-		  text((int) y, 10, ly);
-		}
-	}
-		
-	
+    public void mouseClicked()
+    {
+        for(Star s:stars)
+        {
+            float x = map(s.getxG(), -5, 5, border, width - border);
+            float y = map(s.getyG(), -5, 5, border, height - border);
 
-	public void draw()
-	{	
-		background(0);
-		strokeWeight(2);	
-		drawGrid();	
-	}
+            if (dist(mouseX, mouseY, x, y) < 20)
+            {
+                if (first == null)
+                {
+                    first = s;
+                    break;
+                }
+                else if (second == null)
+                {
+                    second = s;
+                    break;
+                } 
+                else
+                {
+                    first = s;
+                    second = null;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void setup() {
+        colorMode(RGB);
+        loadStars();
+        printStars();
+
+        border = width * 0.1f;
+    }
+
+    public void drawStars()
+    {
+        for(Star s:stars)
+        {
+            s.render(this);
+        }
+    }
+
+    public void draw() 
+    {
+        background(0);
+        drawGrid();
+        drawStars();
+
+        if (first != null)
+        {
+
+            float x = map(first.getxG(), -5, 5, border, width - border);
+            float y = map(first.getyG(), -5, 5, border, height - border);
+
+            if (second != null)
+            {
+                float x2 = map(second.getxG(), -5, 5, border, width - border);
+                float y2 = map(second.getyG(), -5, 5, border, height - border);
+
+                stroke(255, 255, 0);
+                line(x, y, x2, y2);
+
+                float dist = dist(first.getxG(), first.getyG(), first.getzG(), second.getxG(), second.getyG(), second.getzG());
+
+                fill(255);
+                textAlign(CENTER, CENTER);
+                text("Distance between: " + first.getDisplayName() + " and " + second.getDisplayName() + " is " + dist + " parsecs", width / 2, height - (border * 0.5f));
+            }
+            else
+            {
+                stroke(255, 255, 0);
+                line(x, y, mouseX, mouseY);
+            }
+        }
+    }
 }
